00f82193ebb1fb291397d0b77749d1d0bc1986bb
Marcus Smith <marcus.smith@stablekernel.com>
2019-08-17 21:33:37 -0400

    Do it manually

--- Codable.playground/Contents.swift
+++ Codable.playground/Contents.swift
@@ -16,7 +16,39 @@ struct Talk: Codable {
     let speaker: String
     let startTime: Date
     let endTime: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case speaker
+        case startTime
+        case endTime
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decodeIfPresent(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        speaker = try container.decode(String.self, forKey: .speaker)
+        startTime = try container.decode(Date.self, forKey: .startTime)
+        endTime = try container.decode(Date.self, forKey: .endTime)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(speaker, forKey: .speaker)
+        try container.encode(startTime, forKey: .startTime)
+        try container.encode(endTime, forKey: .endTime)
+    }
 }
 
 let decoder = JSONDecoder()
+decoder.dateDecodingStrategy = .iso8601
 let talk = try! decoder.decode(Talk.self, from: jsonData)
+
+let encoder = JSONEncoder()
+encoder.dateEncodingStrategy = .iso8601
+let newData = try! encoder.encode(talk)
+let string = String(bytes: newData, encoding: .utf8)
