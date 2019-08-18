8e9134bc146ebab50b3d7cd51f07b448961066e7
Marcus Smith <marcus.smith@stablekernel.com>
2019-08-17 21:09:16 -0400

    Encode to json

--- Codable.playground/Contents.swift
+++ Codable.playground/Contents.swift
@@ -15,3 +15,6 @@ struct Talk: Codable {
 let codableTalk = try! JSONDecoder().decode(Talk.self, from: jsonData)
 
 let arTalk = Talk(name: "Build immersive experiences with ARKit and CoreLocation", speaker: "Eric Internicola")
+
+let data = try! JSONEncoder().encode(arTalk)
+let string = String(bytes: data, encoding: .utf8)
